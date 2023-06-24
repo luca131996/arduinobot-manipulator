6ebdd4841de6592ccfa257e37c63043b7de00e04
luca131996 <lucacampana13@gmail.com>
2023-06-24 16:56:23 +0200

    Added controller file and its configurations. Updated gitignore.

--- arduinobot_ws/src/arduinobot_controller/include/arduinobot_controller/arduinobot_interface.h
+++ arduinobot_ws/src/arduinobot_controller/include/arduinobot_controller/arduinobot_interface.h
@@ -1,38 +1,43 @@
 #pragma once
 
-#include <hardware_interface/robot_hw.h>
 #include <hardware_interface/joint_state_interface.h>
 #include <hardware_interface/joint_command_interface.h>
+#include <hardware_interface/robot_hw.h>
 #include <controller_manager/controller_manager.h>
 #include <ros/ros.h>
-#include "arduinobot_controller/AnglesConverter.h"
-#include <std_msgs/Uint16MultiArray.h>
-#include <vector>
 #include <boost/scoped_ptr.hpp>
+#include <vector>
+#include <std_msgs/UInt16MultiArray.h>
+#include "arduinobot_controller/AnglesConverter.h"
 
-class ArduinobotInterface : public hardware_interface::RobotHw
-{
-public:
-    ArduinobotInterface(ros::NodeHandle&);
-    void update(const ros::TimerEvent& e); // implementa la logica di controllo
-    void read(); // legge lo stato dei motori
-    void write(ros::Duration elapsed_time); // scrive i risultati su arduino
+class ArduinobotInterface: public hardware_interface::RobotHW {
 
-private:
-    ros::NodeHandle nh;
-    ros::NodeHandle pnh;
-    ros::Duration elapsed_time; // durata in secondi dall'ultima esecuzione del loop di controllo
-    ros::Duration update_freq; // frequenza con cui chiamare la funzione di controllo
-    ros::Timer looper;
-    ros::Publisher hardware_pub;
-    ros::ServiceClient hardware_srv;
-    hardware_interface::JointStateInterface joint_state_interface
-    hardware_interface::PositionJointInterface joint_position_interface
-    boost::shared_ptr<controller_manager::ControllerManager> controller_manager;
-    std::vector<double> cmd; // comando da mandare a ciascun giunto
-    std::vector<double> pos; // posizione
-    std::vector<double> vel; // velocità
-    std::vector<double> eff; // effort di controllo
-    std::vector<std::string> names; // nomi di tutti i giunti 
+    public:
 
+        ArduinobotInterface(ros::NodeHandle&);
+
+        void update(const ros::TimerEvent& e);    
+        void read();
+        void write(ros::Duration elapsed_time);
+    
+    private:
+
+        ros::NodeHandle nh_;
+        ros::NodeHandle pnh_;
+        ros::Duration elapsed_time_;
+        ros::Duration update_freq_;
+        ros::Timer looper_;
+        ros::Publisher hardware_pub_;
+        ros::ServiceClient hardware_srv_;
+
+        hardware_interface::JointStateInterface joint_state_interface_;
+        hardware_interface::PositionJointInterface joint_position_interface_;
+        boost::shared_ptr<controller_manager::ControllerManager> controller_manager_;
+        
+        std::vector<double> cmd_;
+        std::vector<double> pos_;
+        std::vector<double> vel_;
+        std::vector<double> eff_;
+
+        std::vector<std::string> names_;
 };
